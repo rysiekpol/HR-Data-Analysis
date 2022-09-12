dc21171096fbd6fd3cb84e6d51419383dd810053
Maciej Szefler <ma.szefler@gmail.com>
2022-09-13 00:48:38 +0200

    Merging and sorting data

--- HR Data Analysis/task/explore.py
+++ HR Data Analysis/task/explore.py
@@ -2,7 +2,34 @@ import pandas as pd
 import requests
 import os
 
+
 # scroll down to the bottom to implement your solution
+
+def concat_datasets(data_one, data_two):
+    return pd.concat([data_one, data_two])
+
+
+def loading_and_reindexing():
+    office_A = pd.read_xml('../Data/A_office_data.xml')
+    office_B = pd.read_xml('../Data/B_office_data.xml')
+    hr_data = pd.read_xml('../Data/hr_data.xml')
+
+    office_A = office_A.astype({'employee_office_id': str})
+    office_A['employee_office_id'] = 'A' + office_A['employee_office_id']
+    office_A.set_index('employee_office_id', inplace=True)
+
+    office_B = office_B.astype({'employee_office_id': str})
+    office_B['employee_office_id'] = 'B' + office_B['employee_office_id']
+    office_B.set_index('employee_office_id', inplace=True)
+
+    hr_data.set_index('employee_id', inplace=True)
+
+    return office_A, office_B, hr_data
+
+
+def merge_data(AB, HR):
+    return AB.merge(HR, indicator=True, left_index=True, right_index=True)
+
 
 if __name__ == '__main__':
 
@@ -11,8 +38,8 @@ if __name__ == '__main__':
 
     # Download data if it is unavailable.
     if ('A_office_data.xml' not in os.listdir('../Data') and
-        'B_office_data.xml' not in os.listdir('../Data') and
-        'hr_data.xml' not in os.listdir('../Data')):
+            'B_office_data.xml' not in os.listdir('../Data') and
+            'hr_data.xml' not in os.listdir('../Data')):
         print('A_office_data loading.')
         url = "https://www.dropbox.com/s/jpeknyzx57c4jb2/A_office_data.xml?dl=1"
         r = requests.get(url, allow_redirects=True)
@@ -33,21 +60,14 @@ if __name__ == '__main__':
 
         # All data in now loaded to the Data folder.
 
-    office_A = pd.read_xml('../Data/A_office_data.xml')
-    office_B = pd.read_xml('../Data/B_office_data.xml')
-    hr_data = pd.read_xml('../Data/hr_data.xml')
+    office_A, office_B, hr_data = loading_and_reindexing()
 
-    office_A = office_A.astype({'employee_office_id': str})
-    office_A['employee_office_id'] = 'A' + office_A['employee_office_id']
-    office_A.set_index('employee_office_id', inplace=True)
+    concated_AB = concat_datasets(office_A, office_B)
+    merged_AB_HR = merge_data(concated_AB, hr_data)
+    merged_AB_HR.drop(columns='_merge', inplace=True)
+    merged_AB_HR.sort_index(inplace=True)
 
-    office_B = office_B.astype({'employee_office_id': str})
-    office_B['employee_office_id'] = 'B' + office_B['employee_office_id']
-    office_B.set_index('employee_office_id', inplace=True)
+    print(list(merged_AB_HR.index))
+    print(list(merged_AB_HR.columns))
 
-    hr_data.set_index('employee_id', inplace=True)
-
-    print(office_A.index.tolist())
-    print(office_B.index.tolist())
-    print(hr_data.index.tolist())
     # write your code here
